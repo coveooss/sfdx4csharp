ad7a10c6f2d59a1a221eabf5ed0087a3e47aa26f
Benjamin Ross <bross@coveo.com>
2018-09-20 13:22:02 -0400

    Uses the same file folder naming like sfdx-generator and sfdx-js

--- tools/generate-files.js
+++ tools/generate-files.js
@@ -3,7 +3,7 @@ const path = require('path');
 const root = path.resolve(__dirname, './');
 const generator = new sfdxGenerator.Generator({
   SFDXPath: 'sfdx',
-  outputDirectory: path.resolve(root, './src/Client/Commands/Generated'),
+  outputDirectory: path.resolve(root, '../src/Client/Commands/Generated'),
   templateDirectory: path.resolve(root, './classTemplates'),
   fileExtension: '.cs',
   formatFileName: (defaultFileName) => {
